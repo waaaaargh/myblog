8c8352d665fd2784760db231e6f952fbea14b613
Johannes Fürmann <waaaaargh@quantentunnel.de>
2013-02-21 06:05:40 +0100

    adds gitignore

--- blog.py
+++ blog.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python2
 
+import os
+
 from werkzeug.wrappers import Request, Response
+from jinja2 import Environment, FileSystemLoader
 
 from config import Configuration
 import views
@@ -10,23 +13,45 @@ class Blog(object):
     set up blog instance.
     """
     def __init__(self, config_path):
+        # load config
         self.config = Configuration()
         self.config.load_from_file(config_path)
 
+        # initialize jinja templates
+        self.jinja_env = Environment(
+            loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+        )
+
+        # set url maps
         from werkzeug.routing import Map, Rule
         self.url_map = Map([
             Rule('/', endpoint='hello'),
         ])
 
+
+    """
+    handle incoming WSGI requests.
+    """
     def handle_request(self, environment, start_response):
+        # wsgi janitoring
         request = Request(environment)
         adapter = self.url_map.bind_to_environ(request.environ)    
         endpoint, values = adapter.match()
-
+        
+        # execute views
         d = getattr(views, endpoint)(request, environment)
-        response = Response()
+        
+        # get and render template
+        template = self.jinja_env.get_template( endpoint+'.htmljinja' )
+        output = template.render(**d)
+
+        # wsgi janitoring
+        response = Response(output)
         return response(environment, start_response) 
-   
+    
+    """
+    redirect incoming WSGI requests to handle_request
+    """
     def __call__(self, environment, start_response):
         return self.handle_request(environment, start_response)
 
