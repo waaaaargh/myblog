e9f25e44cc3875d4a97a4ea947faa7639a3c2137
Johannes Fürmann <johannes@weltraumpflege.org>
2015-03-24 05:15:40 +0100

    unfucked model, added some public views

--- myblog/model.py
+++ myblog/model.py
@@ -1,47 +1,34 @@
 from myblog import db
-
-post_is_in_category = db.Table('association', db.metadata,
-                               db.Column('post_id', db.Integer,
-                                         db.ForeignKey('posts.id')),
-                               db.Column('category_id', db.Integer,
-                                         db.ForeignKey('categories.id'))
-)
-
+from datetime import datetime
 
 class category(db.Model):
-    __tablename__ = 'categories'
+    __tablename__ = 'category'
+    def __repr__(self):
+        return "Category: \"%s\"" % self.name
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
-    posts = db.relationship("post", secondary=post_is_in_category)
-
-    def __init__(self, name):
-        self.name = name
-
+    posts = db.relationship("post", backref="category")
 
 class post(db.Model):
-    __tablename__ = 'posts'
+    __tablename__ = 'post'
+    def __repr__(self):
+        return "Post \"%s\"" % self.title
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     excerpt = db.Column(db.String)
     content = db.Column(db.String)
     date = db.Column(db.DateTime)
-    categories = db.relationship("category", secondary=post_is_in_category,
-                                 backref='categories')
-    comments = db.relationship("comment", backref="posts")
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    comments = db.relationship("comment", backref="post")
 
-
-class page(db.Model):
-    __tablename__ = 'pages'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    content = db.Column(db.String)
-    lastmodified = db.Column(db.DateTime)
-
+    def __init__(self):
+        db.Model.__init__(self)
+        self.date = datetime.now()
 
 class comment(db.Model):
-    __tablename__ = 'comments'
+    __tablename__ = 'comment'
     id = db.Column(db.Integer, primary_key=True)
-    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
     date = db.Column(db.DateTime)
     name = db.Column(db.String)
     email = db.Column(db.String)
