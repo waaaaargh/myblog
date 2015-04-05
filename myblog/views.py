09a657dd126ea8c1f20d3578f208d1d46fbfb87f
Johannes Fürmann <johannes@weltraumpflege.org>
2015-04-05 16:25:09 +0200

    add user-facing comment functionality * flask-wtf is now a dpendency * add view for writing comments

--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,9 @@
-from myblog import app
-from myblog import model
-from flask import render_template, redirect
+from myblog import app, db, model
+from flask import render_template, redirect, request
 
 from sqlalchemy.orm.exc import NoResultFound
+
+from datetime import datetime
 
 @app.route('/')
 def redirect_to_recent_posts():
@@ -19,7 +20,6 @@ def show_posts(page_number):
     return render_template('list_posts_lastweek.htmljinja', posts=posts)
 
 @app.route('/posts/<int:post_id>', defaults={'slug': ''})
-@app.route('/posts/<int:post_id>/<slug>')
 def single_post_view(post_id, slug):
     try:
         post = model.post.query.\
@@ -29,6 +29,36 @@ def single_post_view(post_id, slug):
     except NoResultFound:
         return "Post not found", 404
     return render_template('post_details.htmljinja', post=post)
+
+@app.route('/posts/<int:post_id>/comment', methods=['POST', 'GET'])
+def add_post_comment(post_id):
+    from wtforms import Form, validators, TextField, TextAreaField, BooleanField
+    from flask.ext.wtf import RecaptchaField
+    class CommentForm(Form):
+        name = TextField('Name', [validators.Length(min=4, max=25)])
+        email = TextField('E-Mail-Addresse', [validators.Length(min=6, max=35)])
+        text = TextAreaField('Dein Kommentar',
+                             [validators.Length(min=30, max=1000)])
+        captcha = RecaptchaField('Bist Du wirklich ein Mensch, und nicht etwa ein Kohlkopf oder irgendsowas?')
+        accept_rules = BooleanField('Ich bin weder ein Nazi noch ein sonstiger Vollidiot', [validators.Required()])
+        
+    form = CommentForm(request.form)
+    post = model.post.query.filter(model.post.id == post_id).first()
+    
+    if request.method == "POST":
+        if form.validate():
+            c = model.comment()
+            c.post = post
+            c.date = datetime.now()
+            c.name = form.name.data
+            c.email = form.email.data
+            c.text = form.text.data
+            db.session.add(c)
+            db.session.commit()
+            return redirect("/posts/%i" % post.id)
+
+    return render_template('post_comment.htmljinja', form=form, post=post)
+    
 
 @app.route('/category/<category_name>')
 def show_category_posts(category_name):
